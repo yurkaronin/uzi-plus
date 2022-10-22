025f7d128c18be8b64324c0ca85003bcbb999bd3
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-10-22 22:16:47 +0300

    Подготовил проект на к вёрстке

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # uzi-plus
  Сайт для компании Узи+ в Смоленске
+
+[шрифт используемый в дизайне: JOST](https://fonts.google.com/share?selection.family=Jost:wght@400;500;600)
+
+Для подключения с серверов гугла [а вдруг ;)]
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+## Список услуг в каталоге
+
+- Акушерство-гинекология
+- Аллергология-иммунология
+- Гастроскопия
+- Гастроэнтерология
+- Кардиология
+- Неврология
+- Онкология-маммология
+- Ортопедия
+- Оториноларингология
+- Педиатрия
+- Психиатрия-наркология
+- Пульмонология
+- Ревматология
+- Сосудистая хирургия
+- Терапия
+- Травматология-ортопедия
+- Ультразвуковая диагностика
+- Урология
+- Функциональная диагностика
+- Хирургия
+- Эндокринология
+
+      akusherstvo-ginekologiya
+      allergologiya-immunologiya
+      gastroskopiya
+      gastroenterologiya
+      kardiologiya
+      nevrologiya
+      onkologiya-mammologiya
+      ortopediya
+      otorinolaringologiya
+      pediatriya
+      psihiatriya-narkologiya
+      pulmonologiya
+      revmatologiya
+      sosudistaya hirurgiya
+      terapiya
+      travmatologiya-ortopediya
+      ultrazvukovaya-diagnostika
+      urologiya
+      funkcionalnaya-diagnostika
+      hirurgiya
+      endokrinologiya
+
+## Слайды с акциями
+
+- Электроэнцефалография
+- Анализы на коронавирус
+- Ведение беременности
+- Видеоэзофагогастродуоденоскопия
+
+      elektroencefalografiya
+      analizy-na-koronavirus
+      vedenie-beremennosti
+      videoezofagogastroduodenoskopiya
---
+++ js/main.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const menuButton = document.querySelector('.nav-button');
+
+  menuButton.addEventListener('click', function () {
+    document.body.classList.toggle('mob-menu-open');
+    menuButton.classList.toggle('open');
+  });
+
+  const swiperIMG = new Swiper('.slider__images', {
+    // параметры слайдера
+    loop: false,
+    speed: 2400,
+    parallax: true,
+    pagination: {
+      el: '.slider__counter-all',
+      type: 'custom',
+      renderCustom: function (swiper, curent, total) {
+        let totalResult = total >= 10 ? total : `0${total}`;
+        return totalResult;
+      }
+    },
+  });
+
+  const swiperDETAILS = new Swiper('.slider__details', {
+    // параметры слайдера
+    loop: false,
+    speed: 2400,
+    parallax: true,
+    grabCursor: true,
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+    mousewheel: {
+      invert: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // on: {
+    //   init() {
+    //     this.el.addEventListener('mouseenter', () => {
+    //       this.autoplay.stop();
+    //     });
+
+    //     this.el.addEventListener('mouseleave', () => {
+    //       this.autoplay.start();
+    //     });
+    //   }
+    // },
+  });
+
+  swiperIMG.controller.control = swiperDETAILS
+  swiperDETAILS.controller.control = swiperIMG
+
+  let gear = document.querySelector('.slider__gear');
+
+  swiperDETAILS.on('slideNextTransitionStart', function () {
+    gsap.to(gear, 2.4, {
+      rotation: '+=40'
+    });
+  });
+  swiperDETAILS.on('slidePrevTransitionStart', function () {
+    gsap.to(gear, 2.4, {
+      rotation: '-=40'
+    });
+  });
+
+  let curent = document.querySelector('.slider__counter-now');
+  let itemNumber = document.querySelector('.slider__item-nember');
+
+  swiperDETAILS.on('slideChange', function () {
+    let slideIndex = swiperDETAILS.realIndex + 1;
+    let indResult = slideIndex >= 10 ? slideIndex : `0${slideIndex}`;
+    gsap.to(curent, 0.2, {
+      force3D: true,
+      y: -20,
+      opacity: 0,
+      ease: Power2.easeOut,
+      onComplete: function () {
+        gsap.to(curent, 0.2, {
+          force3D: true,
+          y: 10,
+        })
+        curent.innerHTML = indResult
+        itemNumber.innerHTML = indResult
+      }
+
+    });
+    gsap.to(curent, 0.2, {
+      force3D: true,
+      y: 0,
+      opacity: 1,
+      ease: Power2.easeOut,
+      delay: 0.6,
+    });
+  });
+
+  MicroModal.init({
+    openTrigger: 'data-micromodal-open',
+    closeTrigger: 'data-micromodal-close',
+    disableFocus: true,
+    disableScroll: true,
+    awaitOpenAnimation: true,
+    awaitCloseAnimation: true,
+
+  });
+
+
+});
+
+
