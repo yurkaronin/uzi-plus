754cb421169b7aeefa4f749b0c4703a8f1033ce3
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-11-10 20:26:44 +0300

    +

---
+++ components/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+
+  <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+  <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  <meta name="author" content="./humans.txt">
+
+  <title>Медицинский центр УЗИ плюс, Смоленск. Официальный сайт</title>
+  <meta name="description" content="Это техническая страница проекта для демонстрации всего списка страниц.">
+
+  <link rel="stylesheet" href="./libs/normalize.min.css">
+  <link rel="stylesheet" href="./libs/micromodal.css">
+  <link rel="stylesheet" href="./libs/swiper-bundle.min.css">
+
+  <link rel="stylesheet" href="./css/style.css">
+
+</head>
