1cc42dcffd8998ab0cf89651318e3e985c123441
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-11-19 17:10:42 +0300

    Доработка адаптива элементов на странице, правки в адаптиве слайдера

--- components/navigation.php
+++ components/navigation.php
@@ -79,6 +79,30 @@
       </li>
     </ul>
 
+    <!-- элементы для мобильных устройств  -->
+    <div class="mobile-elements">
+      <div class="wrapper">
+        <ul class="list-reset">
+          <li>
+            <span>г. Смоленск, ул. Попова, д. 68</span>
+            <a href="tel:+74812605101" class="tel">+7(4812) 60-51-01</a>
+            <a href="tel:+74812615101" class="tel">+7(4812) 61-51-01</a>
+          </li>
+          <li class="mobile-elements__office-hours">
+            График работы:<br> <span>пн-пт 9-19, сб 9-15, вс выходной</span>
+          </li>
+          <li>
+            <span>г. Смоленск, пр. Гагарина, д. 14/2</span>
+            <a href="tel:+74812562457" class="tel">+7(4812) 56-24-57</a>
+            <a href="tel:+74812645101" class="tel">+7(4812) 64-51-01</a>
+          </li>
+        </ul>
+
+      </div>
+
+
+    </div>
+
   </div>
 
 </nav>
