a093f9b29972a777cab0e70d3ba772f937cc774e
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-11-19 18:01:16 +0300

    Секция с рейтингами на главной - разметка и стилизация

---
+++ components/ratings.php
@@ -0,0 +1,40 @@
+<section class="ratings section">
+  <div class="wrapper">
+    <h2 class="title">Рейтинги на независимых сервисах</h2>
+    <div class="ratings__container">
+      <ul class="ratings__list">
+        <li class="ratings__item">
+          <div class="ratings__top-details"><span>3.9</span>
+            <?php include "./components/items/ratings-stars.php"; ?></div>
+
+          <img src="./img/rating-logo/google.png" alt="логотип сервиса" width="100">
+        </li>
+
+        <li class="ratings__item">
+          <div class="ratings__top-details"><span>4.25</span>
+            <?php include "./components/items/ratings-stars.php"; ?></div>
+
+          <img src="./img/rating-logo/prodoctorov.png" alt="логотип сервиса" width="180">
+        </li>
+
+        <li class="ratings__item">
+          <div class="ratings__top-details"><span>4.2</span>
+            <?php include "./components/items/ratings-stars.php"; ?></div>
+
+          <img src="./img/rating-logo/yandex.png" alt="логотип сервиса" width="95">
+        </li>
+
+        <li class="ratings__item">
+          <div class="ratings__top-details"><span>4.25</span>
+            <?php include "./components/items/ratings-stars.php"; ?></div>
+
+          <img src="./img/rating-logo/2gis.png" alt="логотип сервиса" width="100">
+        </li>
+      </ul>
+
+      <div class="image">
+        <img src="./img/artwork/full-examination-of-the-person.svg" alt="">
+      </div>
+    </div>
+  </div>
+</section>
