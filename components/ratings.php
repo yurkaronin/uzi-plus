dab9c5057282721834f97e086d4c97818acd300c
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-11-19 18:44:32 +0300

    Рейтинги - адаптив

--- components/ratings.php
+++ components/ratings.php
@@ -2,39 +2,50 @@
   <div class="wrapper">
     <h2 class="title">Рейтинги на независимых сервисах</h2>
     <div class="ratings__container">
+      <div class="ratings__image">
+        <img src="./img/artwork/full-examination-of-the-person.svg" alt="">
+      </div>
+
       <ul class="ratings__list">
         <li class="ratings__item">
           <div class="ratings__top-details"><span>3.9</span>
             <?php include "./components/items/ratings-stars.php"; ?></div>
 
-          <img src="./img/rating-logo/google.png" alt="логотип сервиса" width="100">
+          <div class="ratings__logo">
+            <img src="./img/rating-logo/google.png" alt="логотип сервиса" width="100">
+          </div>
+
         </li>
 
         <li class="ratings__item">
           <div class="ratings__top-details"><span>4.25</span>
             <?php include "./components/items/ratings-stars.php"; ?></div>
+          <div class="ratings__logo">
+            <img src="./img/rating-logo/prodoctorov.png" alt="логотип сервиса" width="180">
+          </div>
 
-          <img src="./img/rating-logo/prodoctorov.png" alt="логотип сервиса" width="180">
         </li>
 
         <li class="ratings__item">
           <div class="ratings__top-details"><span>4.2</span>
             <?php include "./components/items/ratings-stars.php"; ?></div>
+          <div class="ratings__logo">
+            <img src="./img/rating-logo/yandex.png" alt="логотип сервиса" width="95">
+          </div>
 
-          <img src="./img/rating-logo/yandex.png" alt="логотип сервиса" width="95">
         </li>
 
         <li class="ratings__item">
           <div class="ratings__top-details"><span>4.25</span>
             <?php include "./components/items/ratings-stars.php"; ?></div>
+          <div class="ratings__logo">
+            <img src="./img/rating-logo/2gis.png" alt="логотип сервиса" width="100">
+          </div>
 
-          <img src="./img/rating-logo/2gis.png" alt="логотип сервиса" width="100">
         </li>
       </ul>
 
-      <div class="image">
-        <img src="./img/artwork/full-examination-of-the-person.svg" alt="">
-      </div>
+
     </div>
   </div>
 </section>
