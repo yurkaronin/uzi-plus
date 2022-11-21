1f527fb8820a7256fcfcae0428f7f38d50f16020
RoninsWork <yurkaronin@gmail.com>
2022-11-21 12:38:23 +0300

    Вынес последние секции в отдельные файлы компонента

---
+++ components/faq.php
@@ -0,0 +1,16 @@
+<section class="faq section">
+  <div class="wrapper">
+    <h2 class="title">Часто задаваемые вопросы</h2>
+
+    <ul class="faq__list">
+      <?php 
+        include "./components/items/faq-item.php";
+        include "./components/items/faq-item.php";
+        include "./components/items/faq-item.php";
+        include "./components/items/faq-item.php";
+        include "./components/items/faq-item.php";
+        ?>
+    </ul>
+
+  </div>
+</section>
