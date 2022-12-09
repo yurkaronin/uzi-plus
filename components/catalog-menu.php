10cf47cd000dab0aba55c91f7ec6b3df75e69402
RoninsWork <yurkaronin@gmail.com>
2022-12-09 14:33:04 +0300

    Разметка каталога услуг

---
+++ components/catalog-menu.php
@@ -0,0 +1,110 @@
+<div class="catalog-nav">
+  <ul class="catalog-nav__list">
+    <li class="catalog-nav__item">
+      <a class="catalog-nav__link catalog-nav__link--mod" href="#">
+        <div class="catalog-nav__icon">
+          <img src="./img/services/allergologiya-immunologiya.svg" alt="">
+        </div>
+        <span>Название <small>подтекст</small></span>
+      </a>
+      <ul class="catalog-nav__sublist">
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+      </ul>
+
+    </li>
+    <li class="catalog-nav__item">
+      <a class="catalog-nav__link catalog-nav__link--mod" href="#">
+        <div class="catalog-nav__icon">
+          <img src="./img/services/allergologiya-immunologiya.svg" alt="">
+        </div>
+        <span>Название <small>подтекст</small></span>
+      </a>
+      <ul class="catalog-nav__sublist">
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+      </ul>
+
+    </li>
+    <li class="catalog-nav__item">
+      <a class="catalog-nav__link catalog-nav__link--mod" href="#">
+        <div class="catalog-nav__icon">
+          <img src="./img/services/allergologiya-immunologiya.svg" alt="">
+        </div>
+        <span>Название <small>подтекст</small></span>
+      </a>
+      <ul class="catalog-nav__sublist">
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+      </ul>
+
+    </li>
+    <li class="catalog-nav__item">
+      <a class="catalog-nav__link catalog-nav__link--mod" href="#">
+        <div class="catalog-nav__icon">
+          <img src="./img/services/allergologiya-immunologiya.svg" alt="">
+        </div>
+        <span>Название <small>подтекст</small></span>
+      </a>
+
+      <ul class="catalog-nav__sublist">
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+        <li class="catalog-nav__subitem">
+          <a class="catalog-nav__sublink" href="#">
+            <span>Название</span>
+          </a>
+        </li>
+      </ul>
+
+    </li>
+
+  </ul>
+</div>
