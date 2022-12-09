99236fb63607420331ca54acec01727aaf43e598
RoninsWork <yurkaronin@gmail.com>
2022-12-09 17:03:48 +0300

    Правки от заказчика

--- components/faq.php
+++ components/faq.php
@@ -3,11 +3,42 @@
     <h2 class="title">Часто задаваемые вопросы</h2>
 
     <ul class="faq__list">
+      <li class="faq__item accordion">
+        <div class="accordion__button">
+          <h3 class="accordion__title">Зачем мне звонят из клиники до приема?</h3>
+          <svg width="33" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path class="change" d="M16.5 2.5V31.5" stroke="#41E1BB" stroke-width="4" stroke-linecap="round"
+              stroke-linejoin="round" />
+            <path d="M31 17L2 17" stroke="#41E1BB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <div class="accordion__body">
+          <p>Наши администраторы стараются звонить каждому пациенту, чтобы напомнить о приёме и уточнить, подойдёт ли
+            пациент, не изменились ли у него обстоятельства. Даже если пациент не отвечает на наш звонок, запись
+            сохраняется, врач готов и ждет приёма.</p>
+          <p>Иногда наши пациенты забывают отменить свой приём, лишая возможности других людей проконсультироваться с
+            врачом.
+            Мы будем благодарны, если вы заранее подтвердите свой визит или отмените его.</p>
+        </div>
+      </li>
+
+      <li class="faq__item accordion">
+        <div class="accordion__button">
+          <h3 class="accordion__title">Что будет, если я опоздаю на прием?</h3>
+          <svg width="33" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path class="change" d="M16.5 2.5V31.5" stroke="#41E1BB" stroke-width="4" stroke-linecap="round"
+              stroke-linejoin="round" />
+            <path d="M31 17L2 17" stroke="#41E1BB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <div class="accordion__body">
+          <p>Если вдруг вы задерживаетесь, к сожалению, врач не всегда сможет вас принять, ведь полноценный приём
+            требует определенного количества времени. Однако всегда есть выход, следует предупредить администратора по
+            телефону об опоздании и вам предложат возможные варианты решения проблемы.</p>
+        </div>
+      </li>
+       <!-- Рыба  -->
       <?php 
-        include "./components/items/faq-item.php";
-        include "./components/items/faq-item.php";
-        include "./components/items/faq-item.php";
-        include "./components/items/faq-item.php";
         include "./components/items/faq-item.php";
         ?>
     </ul>
