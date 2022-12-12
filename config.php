f36f7aa90fdc2a284e52afb0972382fa7c9df6d9
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-12-12 23:44:18 +0300

    реализовал вывод faq из БД

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// Подключение к БД 
+$data_base = new PDO('mysql:host=localhost;dbname=uzi_plus', 'root', '');
+// Получаем все элементы таблицы из БД 
+// $sql = "SELECT * FROM `faq`";
+// $result = $data_base->query($sql);
+// Полученные данные из таблицы преобразуем в ассоциативный массив 
+// $faq = $result->fetchAll(PDO::FETCH_ASSOC);
+
+
+// echo '<li>';
+  // echo $item['question'];
+  // echo '<br>';
+  // echo $item['answer'];
+  // echo '</br>';
+
+?>
---
+++ templates/_faq-item.php
@@ -0,0 +1,16 @@
+<li class="faq__item accordion">
+
+  <div class="accordion__button">
+    <!-- вопрос  -->
+    <h3 class="accordion__title"><?php echo$item['question']; ?></h3>
+    <svg width="33" viewBox="0 0 33 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path class="change" d="M16.5 2.5V31.5" stroke="#41E1BB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M31 17L2 17" stroke="#41E1BB" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </div>
+  <!-- ответ  -->
+  <div class="accordion__body"><?php echo$item['answer']; ?></div>
+</li>
+<ul>
+
+</ul>
